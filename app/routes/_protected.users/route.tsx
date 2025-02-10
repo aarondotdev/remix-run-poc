b6f75254882c8b3796c2d80ed1570f2fc286b144
shipshapebear <shipshapebear@gmail.com>
2025-02-11 00:59:35 +0800

    fix nuqs serialize

--- app/routes/_protected.users/route.tsx
+++ app/routes/_protected.users/route.tsx
@@ -5,7 +5,7 @@ import {
   redirect,
   SessionData,
 } from "@remix-run/node";
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import { getSession } from "~/services/session";
 import { fetchData, getHeaders } from "~/lib/fetch-data";
 import { API_BASE_URL, authenticate } from "~/services/authenticate";
@@ -21,6 +21,7 @@ import { Heading } from "~/components/shared/heading";
 import { columns } from "./(components)/columns";
 import { DataTable } from "./(components)/data-table";
 import qs from "qs";
+import { serialize } from "~/lib/search-params";
 
 export const meta: MetaFunction = () => {
   return [
@@ -47,29 +48,24 @@ export const loader: LoaderFunction = async ({ request }) => {
   const filter = searchParams.get("filter[q]") || "";
   const sort = searchParams.get("sort") || "created_at";
 
-  const query = qs.stringify(
-    {
-      "page[number]": Number(pageNumber),
-      "page[size]": Number(pageSize),
-      sort: sort,
-      "filter[q]": filter,
-      "filter[is_player]": false,
-      include: "profile,roles,junketSites",
-    },
-    {
-      encode: false, // Optional: Keeps query params readable
-      arrayFormat: "comma", // Makes arrays compatible with APIs
-    }
-  );
+  const queryString = serialize({
+    "page[number]": Number(pageNumber) || 1,
+    "page[size]": Number(pageSize) || 15,
+    sort: sort,
+    "filter[q]": filter,
+    "filter[is_player]": false,
+    include: "profile,roles,junketSites",
+
+  });
 
   const headers = getHeaders(user.access_token);
-  const url = `${API_BASE_URL}/admin/users/?${query}`;
+  const url = `${API_BASE_URL}/admin/users/${queryString}`;
   const res = await fetchData(url, headers);
   const normalizedData = normalizer(res);
   const data = normalizedData?.data;
   const total = res.meta.total;
 
-  return json({ data, total, query });
+  return json({ data, total, queryString });
 };
 
 const breadcrumbItems = [
@@ -78,19 +74,16 @@ const breadcrumbItems = [
 ];
 
 function route() {
-  const { data, total, query } = useLoaderData<typeof loader>();
-  const revalidator = useRevalidator();
-
-  useEffect(() => {
-    revalidator.revalidate();
-  }, [query]);
+  const { data, total } = useLoaderData<typeof loader>();
 
   return (
     <PageContainer>
       <div className="space-y-4">
         <Breadcrumbs items={breadcrumbItems} />
         <Heading title="Users" description={`Total Items ${total}`} />
+        {/* <Suspense key={queryString} fallback={<DataTableSkeleton />}> */}
         <DataTable columns={columns} data={data} totalItems={total} />
+        {/* </Suspense> */}
       </div>
     </PageContainer>
   );
